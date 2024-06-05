ffb490642056714c242a2360ddc87c5158e398b8
Trykxx <amitnicolas@gmail.com>
2024-06-05 23:16:42 +0200

    modis hover

--- front/src/components/container/header.jsx
+++ front/src/components/container/header.jsx
@@ -1,4 +1,4 @@
-import "../../assets/css/header.css";
+import "./header.css";
 import logo from "../../assets/img/logo-dark.png";
 import {
   Bell,
@@ -12,24 +12,47 @@ import {
 export default function Header() {
   return (
     <header>
-      <img src={logo} alt="Logo Manette du site" className="h-12" />
-      <nav className="flex gap-8 items-center m-auto">
-        <a href="" className="flex items-center gap-2 hover:text-myblue group">
-          <Bell className="group-hover:stroke-myblue"></Bell>Notifications
-        </a>
-        <a href="" className="flex items-center gap-2 hover:text-myblue group">
-          <MessagesSquare className="group-hover:stroke-myblue"></MessagesSquare>Forums
-        </a>
-        <a href="" className="flex items-center gap-2 hover:text-myblue group">
-          <Mail className="group-hover:stroke-myblue"></Mail>Messages
-        </a>
-        <a href="" className="flex items-center gap-2 hover:text-myblue group">
-          <Search className="group-hover:stroke-myblue"></Search>Recherche
-        </a>
-      </nav>
-      <a href="" className="flex items-center gap-2 pr-10 hover:text-myblue group">
-        <CircleUser className=" h-25 group-hover:stroke-myblue"></CircleUser>Profil<ChevronDown className=" h-25 group-hover:stroke-myblue"></ChevronDown>
+      <a href="#">
+        <img src={logo} alt="Logo Manette du site" className="h-12" />
       </a>
+        <nav className="flex gap-8 items-center m-auto s:bg-white">
+          <a
+            href=""
+            className="flex items-center gap-2 hover:text-myblue group"
+          >
+            <Bell className="group-hover:stroke-myblue"></Bell>
+            Notifications
+          </a>
+          <a
+            href=""
+            className="flex items-center gap-2 hover:text-myblue group"
+          >
+            <MessagesSquare className="group-hover:stroke-myblue"></MessagesSquare>
+            Forums
+          </a>
+          <a
+            href=""
+            className="flex items-center gap-2 hover:text-myblue group"
+          >
+            <Mail className="group-hover:stroke-myblue"></Mail>
+            Messages
+          </a>
+          <a
+            href=""
+            className="flex items-center gap-2 hover:text-myblue group"
+          >
+            <Search className="group-hover:stroke-myblue"></Search>
+            Recherche
+          </a>
+        <a
+          href=""
+          className="flex items-center gap-2 pr-10 hover:text-myblue group"
+        >
+          <CircleUser className=" h-25 group-hover:stroke-myblue"></CircleUser>
+          Profil
+          <ChevronDown className=" h-25 group-hover:stroke-myblue"></ChevronDown>
+        </a>
+        </nav>
     </header>
   );
 }
